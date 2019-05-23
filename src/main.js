ff05e908fa5b2582e8d8800e2f0067f8761cd597
pj91 <50563121+pj91@users.noreply.github.com>
2019-05-23 16:08:57 +0900

    First locale commit

--- src/main.js
+++ src/main.js
@@ -5,6 +5,24 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+//vee-validate 추가
+import VeeValidate,{ Validator} from 'vee-validate'
+Vue.use(VeeValidate);
+
+Validator.extend('truth', {
+  getMessage: field => 'The ' + field + ' value is not truthy',
+  validate: value => !! value
+});
+
+let instance = new Validator({ trueField: 'truthy'});
+instance.extend('falsy', (value) => ! value);
+
+instance.attach({
+  name: 'falseField',
+  rules: 'falsy'
+})
+
+
 new Vue({
   router,
   store,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <br>
+
+    <input type="test" v-validate="'required|email'" name="email" placeholder="Enter your email"/><br>
+    <span>{{ errors.first('email') }}</span>
+    
+    <br>
+    <br>
+
+    <input type="text" name="fieldName" v-validate.continues="'required|alpha|min:5'" placeholder="Enter a field name"><br>
+    <span>{{ errors.first('fieldName') }}</span>
+
+
+    <br>
+    <br>
+
+    <input type="text" name="field" v-validate="'falsy'">
   </div>
 </template>
+
